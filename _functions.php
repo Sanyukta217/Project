f9e341da979b02f52887e9f31c16c2a7687c6b31
Sanyukta <sanyukta217@gmail.com>
2022-06-25 16:18:04 +0530

    designed by sanyukta

--- _functions.php
+++ _functions.php
@@ -7,6 +7,7 @@ if(session_id() == '') {
 
 (!isset($_POST['nonce'])) ? die('You are not on a valid location') : "";
 
+// designed by sanyukta
 //To connect with the dbconnection file
 if (!defined('READCON')) {
   define('READCON', true);
